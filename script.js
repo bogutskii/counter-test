f77f485fe10326db8c3a17482554525c5befbf8e
bogutskii <sl9site@gmail.com>
2024-04-11 20:02:50 -0700

    function reset btn reset

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function count() {
+  const countEl = document.getElementById('count');
+  countEl.innerText = parseInt(countEl.innerText) + 1;
+}
+function reset() {
   const countElement = document.getElementById('count');
-  countElement.innerText = parseInt(countElement.innerText) + 1;
+  countElement.innerText = 0;
 }
